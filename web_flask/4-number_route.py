7c1869e62723d4c970ba3fb596d5960b54469b13
Tanagends <tanagends@gmail.com>
2024-02-17 00:36:45 +0200

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Web flask application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """The landing page that returns “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def ctext(text):
+    """returns C followed by the value of the text variable"""
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def ptext (text="is cool"):
+    """Returns Python followed by text variable"""
+    return "Python {}".format(text)
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """displays n if its only a number"""
+    return "{:d} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run()
