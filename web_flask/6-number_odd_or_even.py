211a52164730756eddf376a4f24e58189503e959
Love <mzeelovegeneral@gmail.com>
2024-03-23 18:35:01 +0100

    fix(flask): fixed pycodestyryle error

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """The landing page that returns “Hello HBNB!”"""
@@ -24,7 +25,7 @@ def ctext(text):
 
 @app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def ptext (text="is cool"):
+def ptext(text="is cool"):
     """Returns Python followed by text variable"""
     return "Python {}".format(text)
 
@@ -40,10 +41,12 @@ def template(n):
     """renders template if n is a number"""
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_even(n):
     """renders a template showing whether n is odd or even"""
     return render_template('6-number_odd_or_even.html', n=n)
+
 
 if __name__ == "__main__":
     app.run()
